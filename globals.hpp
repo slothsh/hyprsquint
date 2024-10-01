9ddcb4ccab4926ca4e308ec73f8088c2e912092d
Stefan Olivier <s.olivier1194@gmail.com>
2024-10-01 22:34:25 +0200

    readme + license

--- globals.hpp
+++ globals.hpp
@@ -2,7 +2,6 @@
 
 #include <hyprland/src/plugins/PluginAPI.hpp>
 #include <hyprland/src/SharedDefs.hpp>
-#include <functional>
 
 // Declare global handle pointer
 inline HANDLE HYPR_HANDLE = nullptr;
